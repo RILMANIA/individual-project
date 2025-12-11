af1f7a15b6c145d15c78e21fd0dc5ad170109583
RILMANIA <emir.haikal.eh@gmail.com>
2025-12-11 21:52:35 +0700

    env

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -9,12 +9,9 @@ module.exports = {
         PORT: 80,
 
         // Ini yang ada di file .env kalian
-        JWT_SECRET: "secret",
-        CLOUDINARY_CLOUD_NAME: "dpjqm8kkk",
-        CLOUDINARY_API_KEY: "951827738542538",
-        CLOUDINARY_API_SECRET: "SM1wwWQLTp3ciU_0m0yK1VTyLgI",
+        JWT_SECRET: " ",
         DATABASE_URL:
-          "postgresql://postgres.ivcnayjttcyctzkwziew:X2NoC1he07a95jWT@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
+          "postgresql://postgresql://postgres.ftqkcgvmzayabwpnpidj:bojong99@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres.ivcnayjttcyctzkwziew:X2NoC1he07a95jWT@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
       },
     },
   ],
